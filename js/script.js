cf258bf94aa66602a501c774d5ad0ccdfc06b0ee
Dayne-Game <daynegame024@gmail.com>
2018-08-24 18:23:05 +1200

    Github API

--- js/script.js
+++ js/script.js
@@ -10,3 +10,13 @@ function show_hide_menu() {
 function change_image() {
   toggler.classList.toggle("togglerChange");
 }
+
+/****************************/
+/** This is the Github API **/
+/****************************/
+
+const url = `https://api.github.com/users/dayne-game/repos?per_page=100&client_id=ec569ac44c7094ee0173&client_secret=6a39853312e1467b8331b7106e0be31b9f01250e`;
+
+fetch(url).then(function(response) {
+  return response.json();
+});
