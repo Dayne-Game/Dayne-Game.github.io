53a9b8db94b41bcf2976a4301111dbc91bbb676b
Dayne-Game <daynegame024@gmail.com>
2018-08-27 12:58:06 +1200

    Added Template-3

--- js/script.js
+++ js/script.js
@@ -14,9 +14,3 @@ function change_image() {
 /****************************/
 /** This is the Github API **/
 /****************************/
-
-const url = `https://api.github.com/users/dayne-game/repos?per_page=100&client_id=ec569ac44c7094ee0173&client_secret=6a39853312e1467b8331b7106e0be31b9f01250e`;
-
-fetch(url).then(function(response) {
-  return response.json();
-});
