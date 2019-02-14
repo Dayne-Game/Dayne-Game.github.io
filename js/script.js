fff7c0873666e92070dc3c0984e2900dc4b3e3ff
Dayne-Game <daynegame024@gmail.com>
2019-02-14 20:27:22 +1300

    Fixed Funny navbar color

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ function myFunction() {
     var x = document.querySelector('.link-box');
     if (x.className === "link-box") {
         x.className += " responsive";
+        document.querySelector('.navbar').style.backgroundColor = "#333";
     } else {
         x.className = "link-box";
+        document.querySelector('.navbar').style.backgroundColor = "transparent";
     }
 }
