e7c7cf0a158b15f5cacf6c7700655eb55159cb12
Dayne-Game <daynegame024@gmail.com>
2019-03-10 21:07:48 +1300

    Revamped whole site

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ function myFunction() {
         document.querySelector('.navbar').style.backgroundColor = "transparent";
     }
 }
+
+
+window.onscroll = function () { stickyNav() };
+var nav = document.querySelector('.navbar');
+var sticky = nav.offsetTop;
+
+function stickyNav() {
+    if (window.pageYOffset > sticky) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
+}
+
