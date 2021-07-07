8a1756751ea9b58d4b7db37cff00b05887349210
Dayne Game <daynegame@Daynes-MacBook-Pro.local>
2021-07-07 13:13:13 +1200

    Added New Website

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Grab Sidebar from HTML Pages
+var sidebar = document.querySelector(".popout-sidebar");
+
+// Function that Opens Sidebar
+function openSidebar() {
+  sidebar.style.width = "100%";
+}
+
+// Function that Closes Sidebar
+function closeSidebar() {
+  sidebar.style.width = "0%";
+}
+
+// Function that Closes Overlay
+
+window.setTimeout("closeOverlay()", 4200);
+
+function closeOverlay() {
+  document.querySelector(".overlay").style.display = "none";
+}
